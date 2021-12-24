fb42c0dde9a185343a2a24f3050e9a779010cf07
AwwabSiddiqui <72140015+Awwabsiddiqui@users.noreply.github.com>
2021-12-24 11:59:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,120 +10,142 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 class Main {
-    public static void main(String args[]) {
-
-//        //DATABASE 2 JSON
-//        try {
-//            //Class.forName("com.mysql.cj.jdbc.Driver");
-//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mainer", "root", "16200913");
-//            Statement stmt = conn.createStatement();
-//            ResultSet rs = stmt.executeQuery("select * from empdata");
-//
-//            ResultSetMetaData rsMetaData = rs.getMetaData();
-//            int count = rsMetaData.getColumnCount();
-//            String[] ttl = new String[count + 1];
-//
-//            for (int i = 1; i <=count; i++) {
-//                ttl[i] = (String) rsMetaData.getColumnName(i);
-//                System.out.print(ttl[i] + " ");
-//            }
-//            System.out.println();
-//
-//        //WRITING TO JSON
-//        JSONObject jsonObject = new JSONObject();
-//        JSONArray array = new JSONArray();
-//
-//        while (rs.next()) {
-//            JSONObject record = new JSONObject();
-//            for(int j=1 ; j<ttl.length ; j++){
-//                System.out.print(rs.getString(j) + "  ");
-//                record.put(ttl[j], rs.getString(ttl[j]));
-//            }
-//            System.out.println();
-//            array.add(record);
-//        }
-//        jsonObject.put("DATAFRAME", array);
-//        try {
-//            FileWriter file = new FileWriter("db2json.json");
-//            file.write(jsonObject.toJSONString());
-//            file.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        conn.close();
-//        System.out.println(" JSON FILE CREATED SUCCESSFULLY !!!");
-//    }
-//        catch(Exception e){System.out.println(e);}
 
 
-
-//        //JSON SIMPLE
-//        JSONParser parser = new JSONParser();
-//        try {
-//            Object obj = parser.parse(new FileReader("crunchify.json"));
-//            JSONObject jsonObject = (JSONObject) obj;
-//
-//            JSONArray o = (JSONArray) jsonObject.get("players_data");
-//            System.out.println(o);
-//            System.out.println();
-//            Iterator<JSONObject> iterator2 = o.iterator();
-//            while (iterator2.hasNext()) {
-//                System.out.println(iterator2.next());
-//            }
-//
-//        }
-//         catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-//       //JACKSON
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        try {
-//
-//            // JSON file to Java object
-//            Object staff = mapper.readValue(new FileReader("db2json.json"), Object.class);
-//
-//            // JSON string to Java object
-//            //String jsonInString = "{\"name\":\"mkyong\",\"age\":37,\"skills\":[\"java\",\"python\"]}";
-//            //Object staff2 = mapper.readValue(jsonInString, Object.class);
-//
-//            // compact print
-//            System.out.println(staff);
-//
-//            // pretty print
-//            String prettyStaff1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(staff);
-//
-//            System.out.println(prettyStaff1);
-//
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-            //JACKSON2
-
-        try {
-            // create object mapper instance
-            ObjectMapper mapper = new ObjectMapper();
-
-            // convert JSON file to map
-            Map<?, ?> map = mapper.readValue(new FileReader("db2json.json"), Map.class);
-
-//            for (Map.Entry<?, ?> entry : map.entrySet()) {
-//                System.out.println(entry.getKey() + "=" + entry.getValue());
-//            }
-            System.out.println(map.get("DATAFRAME").getClass());
-             //ArrayList arr =new ArrayList(map.get("DATAFRAME"));
-
-        }
-            catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-
+public static void main(String args[]) {
+        try{
+            System.out.println("1 for db2json    ///    2 for json 2 db");
+            Scanner obj = new Scanner(System.in);
+            int x=obj.nextInt();
+            if (x==1){
+                    dbtojson();
+                    }
+            else if (x==2){
+                jsontodb();
+            }
+            else{
+                System.out.println("Wrong");
+            }
     }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+public static Connection DBconn() throws Exception {
+        //Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mainer", "root", "16200913");
+        return conn;
+    }
+
+public static void dbtojson(){
+        //DATABASE 2 JSON
+        try {
+            Scanner obj = new Scanner(System.in);
+            Connection conn = DBconn();
+            Statement stmt = conn.createStatement();
+            System.out.print("Enter name of databse : ");
+            String dbase = obj.nextLine();
+            ResultSet rs = stmt.executeQuery("select * from "+dbase);
+
+            ResultSetMetaData rsMetaData = rs.getMetaData();
+            int count = rsMetaData.getColumnCount();
+            String[] ttl = new String[count + 1];
+
+            for (int i = 1; i <=count; i++) {
+                ttl[i] = (String) rsMetaData.getColumnName(i);
+                System.out.print(ttl[i] + " ");
+            }
+            System.out.println();
+
+        //WRITING TO JSON
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        while (rs.next()) {
+            JSONObject record = new JSONObject();
+            for(int j=1 ; j<ttl.length ; j++){
+                System.out.print(rs.getString(j) + "  ");
+                record.put(ttl[j], rs.getString(ttl[j]));
+            }
+            System.out.println();
+            array.add(record);
+        }
+        jsonObject.put("DATAFRAME", array);
+        try {
+            FileWriter file = new FileWriter(dbase+".json");
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        conn.close();
+        System.out.println(" JSON FILE CREATED SUCCESSFULLY !!!");
+    }
+        catch(Exception e){System.out.println(e);}
+    }
+
+public static void jsontodb(){
+        try{
+
+            Connection conn = DBconn();
+            Statement stmt = conn.createStatement();
+
+            JSONParser jsonParser = new JSONParser();
+
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("empdata.json"));
+
+            JSONArray jsonArray = (JSONArray) jsonObject.get("DATAFRAME");
+
+            ArrayList<String> arr = new ArrayList<String>();
+
+            int colen=0;
+            for(Object object : jsonArray) {
+                JSONObject record = (JSONObject) object;
+                colen = record.keySet().toArray().length;
+                break;
+            }
+            System.out.println("Length of the columns is : "+colen);
+
+                for(Object object : jsonArray) {
+                    JSONObject record = (JSONObject) object;
+                    for (int i = 0; i < colen; i++){
+                        //System.out.println(record.keySet().toArray()[i]);
+                        arr.add((String)record.keySet().toArray()[i]);
+                }
+                    break;
+                }
+            System.out.println("Column names are : "+arr);
+
+
+            int datalen = jsonArray.toArray().length;
+            System.out.println("Number of Data Rows : "+datalen);
+
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO empdata values (?, ?, ?)");
+
+            for(Object object : jsonArray) {
+                JSONObject record = (JSONObject) object;
+                for(int k=0 ; k<colen ; k++){
+                    //System.out.println(record.get(arr.get(k)));
+                }
+
+                String id = (String) record.get("id");
+                String name = (String) record.get("name");
+                String role = (String) record.get("role");
+
+                pstmt.setString(1,id);
+                pstmt.setString(2,name);
+                pstmt.setString(3,role);
+
+                pstmt.executeUpdate();
+
+            }
+            System.out.println("VALUES INSERTED TO DATABASE SUCCESSFULLY");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
-
-
-
